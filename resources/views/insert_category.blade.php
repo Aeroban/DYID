c8247ed6306c574c48b4b106604f4bd6551286d5
Aeroban <doggy0013@gmail.com>
2021-10-18 22:06:16 +0700

    Category error and css done

--- resources/views/insert_category.blade.php
+++ resources/views/insert_category.blade.php
@@ -3,23 +3,27 @@
 @section('content')
 <div class="insert">
     <div class="insert-form">
-        <p>Insert New Category</p>
+        <p class="insert-form-name">Insert New Category</p>
         <form action="/insert_category" method="POST">
             @csrf
 
             <input type="text" name="name" id="" placeholder="Category name">
             <input type="submit" value="add">
         </form>
-    </div>
 
-    @if ($errors->any())
-        <div class="">
-            {{-- Loop over ALL errors --}}
-            @foreach ($errors->all() as $error)
-                <li style="color: red">{{$error}}</li>
-            @endforeach
+
+        <div class="insert-form-error">
+            @if ($errors->any())
+                <div class="insert-form-error-loop">
+                    {{-- Loop over ALL errors --}}
+                    @foreach ($errors->all() as $error)
+                        <p>{{$error}}</p>
+                        <br> 
+                    @endforeach
+                </div>
+            @endif
         </div>
-    @endif
+    </div>
 </div>
 
 
